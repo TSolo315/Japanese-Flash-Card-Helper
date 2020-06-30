6fba3c90506d82ab702af50dfbf003d8535babe5
Tyler Solosko <TSolo215@live.com>
2020-06-29 23:07:41 -0600

    Fixed line lengths at 100 characters.

--- main.py
+++ main.py
@@ -15,15 +15,18 @@ WAKATI_CONVERTER = WAKATI.getConverter()
 
 
 def get_relevant_data(result_page):
-    """Grabs the relevant information from the term's dictionary page and passes it to the hotkey function to be pasted into Anki."""
+    """Grabs the relevant information from the term's dictionary page and passes it to the hotkey
+     function to be pasted into Anki."""
     kanji = result_page.find("div", class_="jp").text.replace("·", "")
     kanji = re.sub("(\(.{1,3}\))", "", kanji)
-    kana = result_page.find("div", class_="furigana").text.replace("[", "").replace("]", "").replace("·", "")
+    kana = result_page.find(
+        "div", class_="furigana").text.replace("[", "").replace("]", "").replace("·", "")
     kana = re.sub("(\(.{1,3}\))", "", kana)
     romaji = result_page.find("div", class_="romaji hide").text
     term_definition = result_page.find("div", class_="en").find("ol").text.rstrip().lstrip()
     try:
-        example_sentences = result_page.find("div", id="idSampleSentences").find_all("div", class_="sm")
+        example_sentences = result_page.find(
+            "div", id="idSampleSentences").find_all("div", class_="sm")
     except AttributeError:
         print("No example sentences available.")
         prepare_info_hotkey([kanji, kana, romaji, term_definition])
@@ -38,11 +41,13 @@ def get_relevant_data(result_page):
         example_eng = example_sentence[1]
         example_jap_token = WAKATI_CONVERTER.do(example_jap)
         example_jap_kana = KAKASI_CONVERTER.do(example_jap_token)
-        prepare_info_hotkey([kanji, kana, romaji, term_definition, example_jap, example_eng, example_jap_kana])
+        prepare_info_hotkey(
+            [kanji, kana, romaji, term_definition, example_jap, example_eng, example_jap_kana])
 
 
 def initial_search(search_term):
-    """Given a search term or a tanoshiijapense.com search entry link scrape the data from the web page, asking the user to choose the correct term if there are multiple results. Scraped info is
+    """Given a search term or a tanoshiijapense.com search entry link scrape the data from the web
+     page, asking the user to choose the correct term if there are multiple results. Scraped info is
      passed to a function that pulls the desired data from the page."""
     if "tanoshii" not in search_term:
         search_term = 'https://www.tanoshiijapanese.com/dictionary/index.cfm?j=' + search_term
@@ -68,7 +73,10 @@ def initial_search(search_term):
         for i in entries:
             term = i.find("div", class_="jp").text
             definition = i.find("div", class_="en").find("ol").text
-            entry_link = i.find("div", class_="entrylinks").find("a", text='Entry Details »')['href'].replace("../", "https://www.tanoshiijapanese.com/")
+            entry_link = i.find(
+                "div", class_="entrylinks").find(
+                "a", text='Entry Details »')['href'].replace(
+                "../", "https://www.tanoshiijapanese.com/")
             entries_list.append([term, definition, entry_link])
         result_soup = BeautifulSoup(choose_search_term(entries_list).text, "html.parser")
         if not result_soup:
@@ -78,7 +86,8 @@ def initial_search(search_term):
 
 
 def choose_search_term(terms):
-    """Prints the available term options and asks the user to choose which one to proceed with. Returns the chosen data."""
+    """Prints the available term options and asks the user to choose which one to proceed with.
+     Returns the chosen data."""
     print("Which term do you want?\n\n")
     for count, i in enumerate(terms):
         print(str(count + 1) + ": " + i[0] + "\n" + i[1])
@@ -106,7 +115,8 @@ def choose_search_term(terms):
 
 
 def choose_example_sentence(example_sentences):
-    """Prints the available example sentences and asks the user to choose which one to proceed with. Returns the chosen data."""
+    """Prints the available example sentences and asks the user to choose which one to proceed with.
+     Returns the chosen data."""
     print("Choose an example sentence: \n\n")
     for count, i in enumerate(example_sentences):
         print(str(count + 1) + ': ' + i[0] + "\n" + i[1] + "\n")
@@ -123,7 +133,8 @@ def choose_example_sentence(example_sentences):
 
 
 def prepare_info_hotkey(info):
-    """Given a list of information pulled from the search term web page set the information to be pasted into Anki one piece at a time at the press of the specified hotkey."""
+    """Given a list of information pulled from the search term web page set the information to be
+     pasted into Anki one piece at a time at the press of the specified hotkey."""
 
     def write_info(entry):
         keyboard.write(entry)
@@ -138,7 +149,8 @@ def prepare_info_hotkey(info):
 
 
 def main_loop():
-    """Funtion to run on startup that asks the user to provide a search term. Loops on completion."""
+    """Funtion to run on startup that asks the user to provide a search term.
+     Loops on completion."""
     response = input("Enter a search term")
     initial_search(response)
     main_loop()
